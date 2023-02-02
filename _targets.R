a20462bab0f3636a2c9c675a03e7e3034fd179b7
Walker <Leah.Walker@dfo-mpo.gc.ca>
2023-02-02 12:36:32 -0800

    Update _targets.R file

--- _targets.R
+++ _targets.R
@@ -6,7 +6,8 @@ library(tarchetypes)
 tar_option_set(
   packages = c(
     "csasdown",
-    "here"
+    "here",
+    "tibble"
   )
 )
 
@@ -17,50 +18,66 @@ options(
 )
 
 # Source R/
+lapply(list.files("R", full.names = TRUE), source)
 suppressWarnings(tar_source())
+
+file <- here::here("data", "species.csv")
+species <- get_spp_names(file)
+species_tibble <- tibble::as_tibble(species$spp_w_hyphens)
 
 # List targets
 list(
   # Species name nodes ---------------------------------------------------------
-  list(
-    tar_target(
-      species,
-      readr::read_csv(here::here("data", "species.csv")),
-      format = "file"
-    ),
-    tar_target(
-      species_common_name,
-      as.list(species$common_name)
-    ),
-    tar_targets(
-      species_file_name,
-      as.list(species$file_name)
-    ),
-  ),
+  # list(
+  #   tar_target(
+  #     file,
+  #     here::here("data", "species.csv"),
+  #     format = "file"
+  #   ),
+  #   tar_target(
+  #     species,
+  #     get_spp_names(file)
+  #   ),
+  #   tar_target(
+  #     species_tibble,
+  #     as_tibble(species$spp_w_hyphens)
+  #   )
+  # ),
+
   # Define data nodes ----------------------------------------------------------
-  list(
+
     # Define external data path ------------------------------------------------
+ list(
     tar_target(
       data_cache_path,
       here::here("data-cache")
     ),
-    # TODO: Read species data files as nodes
+    # Read species data files as nodes
     tar_map(
-      values = species,
+      values = species_tibble,
       tar_target(
-        species_data,
-        readr::read_rds(paste0(data_cache_path, species_file_name, ".rds")),
-        format = "file"
-      ),
-      list()
-    ),
-    list()
-  ),
-  # Render report --------------------------------------------------------------
-  tar_target(
-    tech_report,
-    csasdown::render(verbose = TRUE),
-    format = "file"
-  ),
-  list()
+      species_data,
+      readr::read_rds(paste0(data_cache_path, "/", value, ".rds"))
+      )
+    )
+  )
 )
+
+
+
+# Render report --------------------------------------------------------------
+#  tar_target(
+#    tech_report,
+#    csasdown::render(verbose = TRUE),
+#    format = "file"
+#  ),
+
+
+# Check for errors
+#tar_manifest(fields = command)
+
+# View dependency graph
+#tar_visnetwork()
+
+# Run the targets
+#tar_make()
