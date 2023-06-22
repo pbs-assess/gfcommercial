379d645dc188e98c20ce4f3fb5e0b19df1c7e7d2
Walker <Leah.Walker@dfo-mpo.gc.ca>
2023-06-22 14:43:47 -0700

    Adjust text size

--- R/plot-samples-gear.R
+++ R/plot-samples-gear.R
@@ -15,7 +15,7 @@ plot_samples <- function(dat,
   # Define years ---------------------------------------------------------------
 
   if (is.null(years)) {
-    years <- seq(min(data$year, na.rm = TRUE), max(data$year, na.rm = TRUE), 1L)
+    years <- seq(min(dat$year, na.rm = TRUE), max(dat$year, na.rm = TRUE), 1L)
   }
 
   year_range = c(min(years), max(years))
@@ -82,8 +82,8 @@ plot_samples <- function(dat,
                                ggplot2::aes(year, value/scale_val,
                                             colour = gear,
                                             fill = gear)
-                               ) +
-      ggplot2::ylim(0, max(stacked_data$samples, na.rm = TRUE) * 1.05)
+                               )
+      #ggplot2::ylim(0, max(stacked_data$samples, na.rm = TRUE) * 1.1)
   }
 
   if (blank_plot) {
@@ -93,13 +93,13 @@ plot_samples <- function(dat,
   g <- g +
     gfplot::theme_pbs() +
     ggplot2::scale_fill_manual(values = pal,
-                      drop = FALSE,
-                      breaks = gears,
-                      labels = gears) +
+                               drop = FALSE,
+                               breaks = gears,
+                               labels = gears) +
     ggplot2::scale_colour_manual(values = pal,
-                        drop = FALSE,
-                        breaks = gears,
-                        labels = gears) +
+                                 drop = FALSE,
+                                 breaks = gears,
+                                 labels = gears) +
     ggplot2::coord_cartesian(xlim = year_range + c(-0.5, 0.5), expand = FALSE) +
     ggplot2::xlab("") +
     ggplot2::ylab(ylab_gg) +
@@ -121,8 +121,8 @@ plot_samples <- function(dat,
       strip.background = ggplot2::element_blank(),
       strip.text.x = ggplot2::element_blank()
     ) +
-    ggplot2::scale_x_continuous(breaks = seq(0, year_range[2], 5))
-    #ggplot2::scale_y_continuous(position = "right", expand = waiver())
+    ggplot2::scale_x_continuous(breaks = seq(0, year_range[2], 5))+
+    ggplot2::scale_y_continuous(limits = c(0, max(stacked_data$samples, na.rm = TRUE) * 1.05), position = "right")
 
   if (blank_plot)
     suppressMessages(g <- g + ggplot2::ylim(0, 1))
@@ -131,9 +131,9 @@ plot_samples <- function(dat,
 
   max_val <- if (!blank_plot) max(gdat[[5]]$ymax) else 1
 
-  labs <- unique(select(samples, area))
+  labs <- unique(select(dat, area))
 
-  .mult <- 0.935 # vertical area label
+  .mult <- 0.91 # vertical area label
 
   g <- g + ggplot2::geom_text(data = labs,
                      x = year_range[1] - 0.2,
@@ -141,14 +141,22 @@ plot_samples <- function(dat,
                      ggplot2::aes(label = area),
                      inherit.aes = FALSE,
                      colour = "grey30",
-                     size = 3,
+                     size = 3.2,
                      hjust = 0
                      )
 
-  g <- g + ggplot2::theme(legend.justification = c(0, 1),
+  g <- g + ggplot2::theme(plot.title = ggplot2::element_text(size = 17),
+                          legend.justification = c(0, 1),
                           legend.position = c(0, 0.2),
+                          legend.text = ggplot2::element_text(size = 9),
                           legend.background = ggplot2::element_blank(),
-                          legend.direction = "horizontal") +
+                          legend.direction = "horizontal",
+                          axis.title.y = ggplot2::element_text(size = 14),
+                          axis.text.x = ggplot2::element_text(size = 12),
+                          axis.text.y.right = ggplot2::element_text(size = 10),
+                          plot.margin = ggplot2::unit(c(0.2, 0.2, 0.5, 0.2), "cm"),
+                          axis.text.y.left = ggplot2::element_blank()
+                          ) +
     ggplot2::guides(fill = ggplot2::guide_legend(nrow = 2)) +
     ggplot2::ggtitle("Commercial specimens")
 
