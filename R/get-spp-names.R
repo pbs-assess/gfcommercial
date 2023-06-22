14a4d979a0451b7ea99065a7cf26667a7691e16b
Walker <Leah.Walker@dfo-mpo.gc.ca>
2023-06-22 14:27:00 -0700

    Add more functions from gfplot

--- R/get-spp-names.R
+++ R/get-spp-names.R
@@ -16,3 +16,29 @@ get_spp_names <- function(file = NULL) {
   stopifnot(sum(duplicated(spp$species_common_name)) == 0)
   tibble::as_tibble(spp)
 }
+
+
+# Function to make species science names in italics ----------------------------
+emph <- function(x) paste0("\\emph{", x, "}")
+
+
+# Function to split up the scientific names for Rougheye -----------------------
+rougheye_split <- function(x) {
+  spl <- strsplit(x, "/")[[1]]
+  first <- strsplit(spl, " ")[[1]][[1]]
+  second <- strsplit(spl, " ")[[1]][[2]]
+  third <- strsplit(spl, " ")[[2]][[1]]
+  c(paste(first, second, sep = "-"), paste(first, third, sep = "-"))
+}
+
+# Assign areas function --------------------------------------------------------
+
+assign_areas <- function(major_stat_area_description,
+                         area_regex = c("3[CD]+", "5[AB]+", "5[CDE]+")) {
+  out <- rep(NA, length(major_stat_area_description))
+  for (i in seq_along(area_regex)) {
+    out[grepl(area_regex[i], major_stat_area_description)] <-
+      gsub("\\^|\\[|\\]|\\+", "", area_regex[i])
+  }
+  out
+}
