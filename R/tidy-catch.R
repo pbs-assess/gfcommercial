3a877c5be8f744a3c60cd9a392a694ed1d05d403
Walker <Leah.Walker@dfo-mpo.gc.ca>
2023-06-22 14:24:53 -0700

    Clean up comments

--- R/tidy-catch.R
+++ R/tidy-catch.R
@@ -1,57 +1,13 @@
-# Modified tidy catch function from gfplot
-# credit to Sean Anderson
+# Modified tidy catch function from gfplot - Sean Anderson
 
-
-# Set fishing year (not needed) ------------------------------------------------
-set_fish_year <- function(dat,
-                             month_fishing_starts = 1,
-                             day_fishing_starts = 1,
-                             yr_col = "year",
-                             date_col = c("best_date", "trip_start_date"),
-                             ...) {
-  stopifnot(yr_col %in% names(dat))
-  stopifnot(date_col %in% names(dat))
-  stopifnot(month_fishing_starts %in% 1:12)
-  stopifnot(day_fishing_starts %in% 1:31)
-  if (month_fishing_starts == 2 && day_fishing_starts > 28) {
-    stop("day_fishing_starts must be 28 or less for February", call. = FALSE)
-  }
-  if (month_fishing_starts %in% c(4, 6, 9, 11) && day_fishing_starts > 30) {
-    stop("day_fishing_starts must be 30 or less for April, June, September, or November", call. = FALSE)
-  }
-
-  yr_col_sym <- sym(yr_col)
-  date_col_sym <- sym(date_col)
-
-  dat %>%
-    mutate(
-      day_of_year = lubridate::yday(!!date_col_sym),
-      cutoff_day = lubridate::yday(lubridate::ymd(paste0(!!yr_col_sym, "-", month_fishing_starts, "-", day_fishing_starts))),
-      !!yr_col_sym := ifelse(day_of_year < cutoff_day, year - 1, year)
-    ) %>%
-    select(-day_of_year, -cutoff_day)
-}
-
-
-# Assign areas -----------------------------------------------------------------
-
-assign_areas <- function(major_stat_area_description,
-                         area_regex = c("3[CD]+", "5[AB]+", "5[CDE]+")) {
-  out <- rep(NA, length(major_stat_area_description))
-  for (i in seq_along(area_regex)) {
-    out[grepl(area_regex[i], major_stat_area_description)] <-
-      gsub("\\^|\\[|\\]|\\+", "", area_regex[i])
-  }
-  out
-}
-
-
-# Tidy catch -------------------------------------------------------------------
+# Tidy catch function ----------------------------------------------------------
+# a function to tidy the catch data by gear type and by area or total
 
 my_tidy_catch <- function(dat,
                           areas = NULL,
                           ...) {
 
+  # Assign areas (or total)
   if (!is.null(areas)) {
     dat$area <- assign_areas(dat$major_stat_area_name, areas)
     dat <- dat[!is.na(dat$area), , drop = FALSE]
@@ -59,6 +15,7 @@ my_tidy_catch <- function(dat,
     dat$area <- "Total"
   }
 
+  # Summarize catch data
   dat <- filter(dat, !is.na(species_common_name), !is.na(year)) %>%
     group_by(year, species_common_name, gear, area) %>%
     summarise(
@@ -72,6 +29,8 @@ my_tidy_catch <- function(dat,
 
   # Note that discarded_kg only includes trawl discards from 1996+ and
   # trap/hook and line discards from 2006+
+
+  # Rename gear types
   catches <- mutate(dat,
                     gear = dplyr::recode(gear,
                                          UNKNOWN = "Unknown/trawl",
@@ -89,19 +48,20 @@ my_tidy_catch <- function(dat,
                        id.vars = c("year", "species_common_name", "area", "gear")
   )
 
+  # Rename gear for all discards as 'discarded'
   landings <- filter(cm, variable %in% c("landed_kg"))
   discards <- filter(cm, variable %in% c("discarded_kg"))
-
   landings$gear <- as.character(landings$gear)
   discards$gear <- as.character(discards$gear)
   discards$gear <- "Discarded"
-
   all_catch <- bind_rows(landings, discards)
+
   # Make a vector of new levels based on what is actually in the data
   # Without this, pre-filtered catch data may cause an error if some
   # of the gear types are missing in the table.
   # The levels will follow the order of `all_gear_ordered` for present
   # gear types, missing ones are not included in the levels
+
   gears_present <- unique(all_catch$gear)
   all_gear_ordered <- c("Bottom trawl",
                         "Midwater trawl",
@@ -111,9 +71,7 @@ my_tidy_catch <- function(dat,
                         "Discarded")
   relevel_gears <- gears_present[order(match(gears_present, all_gear_ordered))]
   all_catch <- mutate(all_catch,
-                      gear = forcats::fct_relevel(
-                        gear, relevel_gears
-                      )
+                      gear = forcats::fct_relevel(gear, relevel_gears)
   )
 
   all_catch <- group_by(all_catch, year, species_common_name, area, gear) %>%
@@ -124,17 +82,16 @@ my_tidy_catch <- function(dat,
 }
 
 
-# Total catch ------------------------------------------------------------------
+# Total catch function ---------------------------------------------------------
+# a function to amalgamate the tidied catch data by area and total
 
 catch_total <- function(dat, years = NULL, ...) {
 
-  # Define area factor levels --------------------------------------------------
-
+  # Define area factor levels
   area_levels <- c("5E", "5D", "5C", "5B", "5A", "3D", "3C", "4B", "Total")
 
 
-  # Create a 'Total' area ------------------------------------------------------
-
+  # Tidy catch data by areas and for total and combine the data frames
   catch_areas <- my_tidy_catch(dat,
                                areas = c("5E", "5D", "5C", "5B", "5A", "3D", "3C", "4B"),
                                ...)
@@ -146,19 +103,16 @@ catch_total <- function(dat, years = NULL, ...) {
   catch <- catch %>%
     dplyr::mutate(area = factor(area, levels = area_levels))
 
-  # Define years ---------------------------------------------------------------
-
+  # Define years
   if (is.null(years)) {
     years <- seq(min(catch$year, na.rm = TRUE), max(catch$year, na.rm = TRUE), 1L)
   }
 
-  # Filter by years ------------------------------------------------------------
-
+  # Filter by years
   catch <- catch %>%
     dplyr::filter(year %in% years)
 
-  # Fill in missing values with NA ---------------------------------------------
-
+  # Fill in missing values with NA
   gear <- c("Bottom trawl",
             "Midwater trawl",
             "Hook and line",
